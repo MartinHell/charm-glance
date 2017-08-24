2295ba2f2efacef3b54afcb441ad4e180ab70bd6
Martin Hellstrom <martin.hellstrom@dgc.se>
2017-08-24 16:27:55 +0200

    created config option for json logs

--- hooks/glance_contexts.py
+++ hooks/glance_contexts.py
@@ -163,6 +163,7 @@ class GlanceLoggingContext(OSContextGenerator):
 
     def __call__(self):
         ctxt = {}
+        ctxt['json_logs'] = config('json-logs')
         debug = config('debug')
         if debug:
             ctxt['root_level'] = 'DEBUG'
